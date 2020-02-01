442bc6d8b2c2c2b379892bfef9f4dd70bba9ab9a
Йошкарали <dashkevich672@gmail.com>
2020-02-01 12:30:09 +0200

    Updates

---
+++ precache-manifest.09a491f63ae8aca98217955e9d1c2443.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f76b3c086770b781662722f2572bbf",
+    "url": "/goit-react-hw-06-phonebook-redux-toolkit/index.html"
+  },
+  {
+    "revision": "193cfb16eab6a83921ff",
+    "url": "/goit-react-hw-06-phonebook-redux-toolkit/static/css/main.06f642bc.chunk.css"
+  },
+  {
+    "revision": "5a535836c88fca7015af",
+    "url": "/goit-react-hw-06-phonebook-redux-toolkit/static/js/2.da41db95.chunk.js"
+  },
+  {
+    "revision": "193cfb16eab6a83921ff",
+    "url": "/goit-react-hw-06-phonebook-redux-toolkit/static/js/main.0d2abd75.chunk.js"
+  },
+  {
+    "revision": "57ce4777056eb69c10a4",
+    "url": "/goit-react-hw-06-phonebook-redux-toolkit/static/js/runtime-main.92ffc697.js"
+  },
+  {
+    "revision": "efc182efb7163e84342f50f4b395c976",
+    "url": "/goit-react-hw-06-phonebook-redux-toolkit/static/media/alert.efc182ef.png"
+  }
+]);
